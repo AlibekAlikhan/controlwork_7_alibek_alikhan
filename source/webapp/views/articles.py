df7f07bb709cd44ec7f2ee8c193cc5d3748ac8f5
Alibek Alikhan <alialikhan0520@gmail.com>
2023-02-25 13:38:47 +0600

    Добавил функционалы редактирования, добавления, удаления

--- source/webapp/views/articles.py
+++ source/webapp/views/articles.py
@@ -5,9 +5,11 @@ from webapp.models import Article
 
 from webapp.forms import ArticleForm
 
+from webapp.models import StatusChoice
+
 
 def article_view(request: WSGIRequest):
-    articles = Article.objects.exclude(is_deleted=True)
+    articles = Article.objects.filter(status=StatusChoice.ACTIVE).order_by("-create_at")
     context = {
         "articles": articles
     }
@@ -23,26 +25,20 @@ def article_create(request: WSGIRequest):
         return render(request, "article_create.html", context={"form": form})
     else:
         article = Article.objects.create(**form.cleaned_data)
-        return redirect("detail_view", pk=article.pk)
+        return redirect("index_article")
 
 
 def article_update(request: WSGIRequest, pk):
     article = get_object_or_404(Article, pk=pk)
+    article.update()
     if request.method == "POST":
         form = ArticleForm(request.POST, instance=article)
         if  form.is_valid():
             form.save()
-            return redirect("detail_view", pk=article.pk)
+            return redirect("index_article")
         return render(request, "article_update.html", context={"form": form, "article": article})
     form = ArticleForm(instance=article)
     return render(request, "article_update.html", context={"form": form, "article": article})
-
-
-def detail_view(request: WSGIRequest, pk):
-    article = get_object_or_404(Article, pk=pk)
-    return render(request, "detail_article.html", context={
-        'article': article
-    })
 
 
 def deleted(request: WSGIRequest, pk):
